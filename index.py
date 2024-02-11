221f6c342627c376259534521baefb2d86999c98
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-10 22:18:43 -0500

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ def generar_clave():
     Genera una clave para la encriptación.
     """
     return Fernet.generate_key()
+def encriptar_datos(datos, clave):
+    """
+    Encripta los datos utilizando una clave proporcionada.
+    """
